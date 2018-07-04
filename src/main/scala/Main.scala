fad9d867901da79b8d9ce36a1cb9acf5c2889fa0
cw-harada <harada@chatwork.com>
2018-07-04 13:45:10 +0900

    create model and merge template

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,35 +1,27 @@
 import java.io.{File, OutputStreamWriter, Writer}
-import scala.collection.JavaConverters._
-import freemarker.template.{Configuration, Template, TemplateExceptionHandler}
 
-import scala.collection.immutable.HashMap
+import freemarker.template.{Configuration, Template, TemplateExceptionHandler}
+import model.Product
 
 object Main extends App {
 
-
-  /* ------ ------------------------------------------------------------------ */
-  /* You should do this ONLY ONCE in the whole application life-cycle:        */
-
-  /* Create and adjust the configuration singleton */
   val cfg = new Configuration(Configuration.VERSION_2_3_27)
-  cfg.setDirectoryForTemplateLoading(new File("/Users/haradashingo/raml/templates"))
+  val templateDirectory = new java.io.File(".").getCanonicalPath + "/templates"
+  cfg.setDirectoryForTemplateLoading(new File(templateDirectory))
   cfg.setDefaultEncoding("UTF-8")
   cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER)
   cfg.setLogTemplateExceptions(false)
   cfg.setWrapUncheckedExceptions(true)
 
-  /* ------------------------------------------------------------------------ */
-  /* You usually do these for MULTIPLE TIMES in the application life-cycle:   */
+  val temp: Template = cfg.getTemplate("test.ftlh")
 
-  /* Create a data-model */
-  val root: java.util.Map[String, String] = HashMap("user" -> "Big Joe").asJava
+  val root: java.util.HashMap[String, Object] = new java.util.HashMap[String, Object]
 
-  /* Get the template (uses cache internally) */
-  val temp: Template = cfg.getTemplate("test.ftlh");
+  val latest = new Product("http://example.com", "Click here")
 
-  /* Merge data-model with template */
-  val out: Writer = new OutputStreamWriter(System.out);
-  temp.process(root, out);
-  // Note: Depending on what `out` is, you may need to call `out.close()`.
-  // This is usually the case for file output, but not for servlet output.
+  root.put("product", latest)
+
+  val out: Writer = new OutputStreamWriter(System.out)
+  temp.process(root, out)
+
 }
---
+++ src/main/scala/model/Product.scala
@@ -0,0 +1,3 @@
+package model
+
+case class Product(url: String, name: String)
