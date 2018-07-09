0858bf986c344596905ab2b1d840fb2a6ff49b1b
cw-harada <harada@chatwork.com>
2018-07-09 11:52:14 +0900

    apply cap first

--- output/Client.scala
+++ output/Client.scala
@@ -3,7 +3,7 @@ import com.softwaremill.sttp._
 class apiClient(val baseUri: String = "https://api.chatwork.com/", val version: String = "v2") {
 
   // get the job
-  def getJob(job_id: integer = 30) = {}
+  def getJob(job_id: Integer = 30) = {}
 
   // create a new job
   def createJob() = { val response = sttp.post(baseUri).body({ "id" : 5, "name" : "new job name" } ).send(); println(response.body) }
